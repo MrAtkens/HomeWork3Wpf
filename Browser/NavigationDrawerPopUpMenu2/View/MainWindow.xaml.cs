3910e81229c0830252bcbeac940a778acad46904
MrAtkens <r.kaliaskar@mail.ru>
2019-02-10 02:25:56 +0600

    HomeWork3Wpf
    HomeWork3Wpf

---
+++ Browser/NavigationDrawerPopUpMenu2/View/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+
+namespace BrowserOnWpf
+{
+
+
+	public partial class MainWindow : Window
+	{
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+		}
+
+		private void ButtonSearchClick(object sender, RoutedEventArgs e)
+		{
+			browserWindow.Navigate(new Uri("http://" + selectSite.Text));
+		}
+
+		private void MinimazeClick(object sender, RoutedEventArgs e)
+		{
+			WindowState = WindowState.Minimized;
+		}
+
+		private void MaximazeClick(object sender, RoutedEventArgs e)
+		{
+			if (WindowState == WindowState.Normal)
+			{
+				WindowState = WindowState.Maximized;
+			}
+			else if (WindowState == WindowState.Maximized)
+			{
+				WindowState = WindowState.Normal;
+			}
+		}
+
+		private void ExitClick(object sender, RoutedEventArgs e)
+		{
+			System.Windows.Application.Current.Shutdown();
+		}
+
+		private void WindowDragMove(object sender, System.Windows.Input.MouseButtonEventArgs e)
+		{
+			DragMove();
+		}
+	}
+
+}
+
---
+++ CalculatorUI/CalculatorUI/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorUI
+{
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void NumberOrOperationButtonClick(object sender, RoutedEventArgs e)
+		{
+
+			if (outPutField.Text == "Press...")
+			{
+				outPutField.Text = "";
+				Button bufferButton = (Button)sender;
+				outPutField.Text += bufferButton.Content.ToString();
+			}
+
+			else if (outPutField.Text == "")
+			{
+				outPutField.Text = "Press...";
+			}
+			else
+			{
+				Button bufferButton = (Button)sender;
+				outPutField.Text += bufferButton.Content.ToString();
+			}
+
+		}
+
+		private void EquallyClick(object sender, RoutedEventArgs e)
+		{
+			try
+			{
+				result();
+			}
+			catch (Exception exc)
+			{
+				outPutField.Text = "Error!";
+			}
+		}
+
+		private void result()
+		{
+			String oprationString;
+			int operation = 0;
+			if (outPutField.Text.Contains("+"))
+			{
+				operation = outPutField.Text.IndexOf("+");
+			}
+			else if (outPutField.Text.Contains("-"))
+			{
+				operation = outPutField.Text.IndexOf("-");
+			}
+			else if (outPutField.Text.Contains("X"))
+			{
+				operation = outPutField.Text.IndexOf("X");
+			}
+			else if (outPutField.Text.Contains("/"))
+			{
+				operation = outPutField.Text.IndexOf("/");
+			}
+			else if (outPutField.Text.Contains("√"))
+			{
+				operation = outPutField.Text.IndexOf("√");
+			}
+			else if (outPutField.Text.Contains("%"))
+			{
+				operation = outPutField.Text.IndexOf("%");
+			}
+			else
+			{
+				//error    
+			}
+
+			oprationString = outPutField.Text.Substring(operation, 1);
+			double op1 = Convert.ToDouble(outPutField.Text.Substring(0, operation));
+			double op2 = Convert.ToDouble(outPutField.Text.Substring(operation + 1, outPutField.Text.Length - operation - 1));
+
+			if (oprationString == "+")
+			{
+				outPutField.Text += "=" + (op1 + op2);
+			}
+			else if (oprationString == "-")
+			{
+				outPutField.Text += "=" + (op1 - op2);
+			}
+			else if (oprationString == "*")
+			{
+				outPutField.Text += "=" + (op1 * op2);
+			}
+			else if (oprationString == "/")
+			{
+				outPutField.Text += "=" + (op1 / op2);
+			}
+			else if (oprationString == "%")
+			{
+				outPutField.Text += "=" + (op1 * op2/100);
+			}
+			else if (oprationString == "√")
+			{
+				outPutField.Text += "=" + (Math.Sqrt(op1));
+			}
+		}
+
+		private void ButtonDeleteAllClick(object sender, RoutedEventArgs e)
+		{
+			outPutField.Text = "";
+		}
+
+		private void ButtonDeleteClick(object sender, RoutedEventArgs e)
+		{
+			if (outPutField.Text.Length > 0)
+			{
+				outPutField.Text = outPutField.Text.Substring(0, outPutField.Text.Length - 1);
+			}
+		}
+
+	}
+}
